1a5e698a471f445a465961824105ecb58d07d99a
Thies Stettler <94679617+ThiesTB@users.noreply.github.com>
2025-06-21 14:51:53 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+import gspread
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
+
+# ... rest of the updated script is expected to be restored by user from version history
